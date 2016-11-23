c1545eed0bf269d8d4807f926f34ad110238d697
czross <czr@uw.edu>
2016-11-22 20:13:06 -0800

    init

---
+++ EmtDoc/EmtDoc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EmtDoc
+//
+//  Created by Brian Ross on 11/22/16.
+//  Copyright © 2016 FormatHealth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
