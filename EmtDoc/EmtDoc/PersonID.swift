b05411d03b39d76919d5cd609e28d6e5b785bb0c
peter <peterlu195@yahoo.com>
2016-11-30 15:30:11 -0800

    implement patient name and dob in personID class

--- EmtDoc/EmtDoc/PersonID.swift
+++ EmtDoc/EmtDoc/PersonID.swift
@@ -8,6 +8,11 @@
 
 import Foundation
 
+extension String {
+    func trim() -> String {
+        return self.trimmingCharacters(in: NSCharacterSet.whitespaces)
+    }
+}
 
 public class PersonID {
     public var fName : String = ""
@@ -18,12 +23,62 @@ public class PersonID {
     public var gender : String = ""
     
     // TODO: Date class for dob
-    public var dob : String = ""
+    public var dob : Date?
     public var age: Int = -1
+    public var weight : Double = 0.0
+    public var race : String = ""
+    public var advanced : String = ""
     
+    public var address : String = ""
+    public var city : String = ""
+    public var state : String = ""
+    public var zip : String = ""
+    public var telephone : String = ""
+    public var physician : String = ""
+    
+    public var medications = Set<String>()
+    public var allergies = Set<String>()
     
     
     public init () {
         
     }
+    
+    func set(fName : String) {
+        self.fName = fName.trim()
+    }
+    
+    func set(lName : String) {
+        self.lName = lName.trim()
+    }
+    
+    func set(middleInitial : String) {
+        var newMiddleInitial = middleInitial
+        newMiddleInitial = newMiddleInitial.trim()
+        if let letter = newMiddleInitial.characters.first {
+            self.middleInitial = String(letter)
+        } else {
+            self.middleInitial = ""
+        }
+    }
+    
+    func set(gender : String) {
+        self.gender = gender
+    }
+    
+    func set(dobMonth : Int, dobDay : Int, dobYear : Int) {
+        
+        // Initialize Date components
+        let components = NSDateComponents()
+        components.month = dobMonth
+        components.day = dobDay
+        components.year = dobYear
+        
+        // Get NSDate given the above date components
+        let date = NSCalendar(identifier: NSCalendar.Identifier.gregorian)?.date(from: components as DateComponents)
+        
+        self.dob = date!
+    }
 }
+
+
