24b0fe4dc36edaa91fa4e1a71fba1c6e1e581fbc
peter <peterlu195@yahoo.com>
2016-11-29 19:42:15 -0800

    create personID stub

--- EmtDoc/EmtDoc/PersonID.swift
+++ EmtDoc/EmtDoc/PersonID.swift
@@ -7,3 +7,23 @@
 //
 
 import Foundation
+
+
+public class PersonID {
+    public var fName : String = ""
+    public var lName : String = ""
+    public var middleInitial : String = ""
+    
+    // TODO: Enumerate Gender
+    public var gender : String = ""
+    
+    // TODO: Date class for dob
+    public var dob : String = ""
+    public var age: Int = -1
+    
+    
+    
+    public init () {
+        
+    }
+}
