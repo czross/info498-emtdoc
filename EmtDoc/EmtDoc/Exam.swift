231790e000b2fa1f2b5fb967b736e0b1475d1425
MattYi <hzyi@uw.edu>
2016-12-04 14:27:42 -0800

    finish exam.swift

--- EmtDoc/EmtDoc/Exam.swift
+++ EmtDoc/EmtDoc/Exam.swift
@@ -7,3 +7,47 @@
 //
 
 import Foundation
+
+public class Exam {
+  
+  public var mentalStatus: String
+  public var skin: String
+  public var facial: [String: String]
+  public var chest: [String: String]
+  public var abdomen: String
+  public var back: String
+  public var pelvic: String
+  public var extremities: [String: String]
+  public var neuro: String
+  
+  public init() {
+    mentalStatus = "Unspecified"
+    
+    skin = "Unspecified"
+    
+    facial = [String: String]()
+    facial["pupils"] = "Unspecified"
+    facial["throat"] = "Unspecified"
+    facial["head"] = "Unspecified"
+    facial["ears"] = "Unspecified"
+    facial["nose"] = "Unspecified"
+    
+    chest = [String: String]()
+    chest["heartSounds"] = "Unspecified"
+    chest["lungs"] = "Unspecified"
+    
+    back = "Unspecified"
+    
+    abdomen = "Unspecified"
+    
+    pelvic = "Unspecified"
+    
+    neuro = "Unspecified"
+    
+    extremities = [String: String]()
+    extremities["leftUpper"] = "Unspecified"
+    extremities["leftLower"] = "Unspecified"
+    extremities["righttUpper"] = "Unspecified"
+    extremities["rightLower"] = "Unspecified"
+  }
+}
