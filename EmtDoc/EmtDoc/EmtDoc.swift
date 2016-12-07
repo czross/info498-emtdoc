9bc9ce1b74a97df6bb50291bbd358b76eaa980aa
czross <czr@uw.edu>
2016-12-06 18:29:17 -0800

    add Overall Model and Repo file

---
+++ EmtDoc/EmtDoc/EmtDoc.swift
@@ -0,0 +1,78 @@
+//
+//  EmtDoc.swift
+//  EmtDoc
+//
+//  Created by Brian Ross on 12/6/16.
+//  Copyright © 2016 FormatHealth. All rights reserved.
+//
+
+import Foundation
+
+class EmtDoc {
+    let person: PersonID = PersonID()
+    let exam: Exam = Exam()
+    let procedures: Procedure = Procedure()
+    let vitals: VitalSigns = VitalSigns()
+    let glascow: GlascowDscp = GlascowDscp()
+    
+    let mainChoices: [String] = [
+        "Chief Complaint",
+        "Vital Signs",
+        "Exam",
+        "Procedures/Medications"
+    ]
+    
+    let personChoices: [String] = [
+        "First Name",
+        "Last Name",
+        "Middle Initial",
+        "Gender",
+        "Date of Birth",
+        "Age",
+        "Weight (Kg)",
+        "Advanced",
+        "Address",
+        "City",
+        "State",
+        "Zip",
+        "Telephone",
+        "Physician",
+        "Medications",
+        "Allergies"
+    ]
+    
+    let examChoice: [String] = [
+        "Mental Status",
+        "Skin",
+        "Facial",
+        "Chest",
+        "Abdomen",
+        "Back",
+        "Pelvis",
+        "Extremities",
+        "Neuro"
+    ]
+    
+    let vitalChoices: [String] = [
+        "Weight",
+        "Blood Pressure",
+        "Heart Rate",
+        "Respiratory Rate",
+        "02 Saturation",
+        "End Tidal CO2",
+        "Temperature",
+        "Pain Scale",
+        "Glascow"
+    ]
+    
+    let procedureChoice: [String] = [
+        "Medications",
+        "Procedures",
+        "Splints"
+    ]
+    
+    
+    
+    init() {
+    }
+}
---
+++ EmtDoc/EmtDoc/Repository.swift
@@ -0,0 +1,13 @@
+//
+//  Repository.swift
+//  EmtDoc
+//
+//  Created by Brian Ross on 12/6/16.
+//  Copyright © 2016 FormatHealth. All rights reserved.
+//
+
+import Foundation
+
+class Repository {
+    
+}
