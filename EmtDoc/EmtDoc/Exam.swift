0e723d0c9b1d718834b154f23e14c17908400cfe
MattYi <hzyi@uw.edu>
2016-12-07 19:10:24 -0800

    Finish exam

--- EmtDoc/EmtDoc/Exam.swift
+++ EmtDoc/EmtDoc/Exam.swift
@@ -20,6 +20,17 @@ public class Exam {
   public var extremities: [String: String]
   public var neuro: String
   
+  
+  public var mentalStatusChoices: [String]
+  public var skinChoices: [String]
+  public var facialChoices: [String: [String]]
+  public var chestChoices: [String: [String]]
+  public var abdomenChoices: [String]
+  public var backChoices: [String]
+  public var pelvicChoices: [String]
+  public var extremityChoices: [String: [String]]
+  public var neuroChoices: [String]
+
   public init() {
     mentalStatus = "Unspecified"
     
@@ -49,5 +60,33 @@ public class Exam {
     extremities["leftLower"] = "Unspecified"
     extremities["righttUpper"] = "Unspecified"
     extremities["rightLower"] = "Unspecified"
+    
+    mentalStatusChoices = ["Alert", "Oriented", "Unintelligible", "Unresponsive"]
+    
+    skinChoices = ["Normal", "Pale", "Mottled"]
+    facialChoices = [String: [String]]()
+    facialChoices["pupils"] = ["Equal", "Unequal", "Responsive", "Fixed", "Dilated"]
+    facialChoices["Ears"] = ["No Blood", "Blood", "Clear Fluid"]
+    facialChoices["Nose"] = ["No Blood", "Blood", "Clear Fluid"]
+    facialChoices["Throat"] = ["Normal", "Injury"]
+    
+    chestChoices = [String: [String]]()
+    chestChoices["Lungs"] = ["Bilateral Breath Sounds", "L unclear", "R unclear"]
+
+    abdomenChoices = ["Vomiting", "Normal", "Left upper quadrant – normal",
+                      "Right upper quadrant – normal", "Left lower quadrant – normal",
+                      "Right lower quadrant – normal"]
+
+    backChoices = ["Cervical", "Thoracic", "Lumbar"]
+    
+    extremityChoices = [String: [String]]()
+    extremityChoices["leftUpper"] = ["Normal", "NotNormal"]
+    extremityChoices["rightUpper"] = ["Normal", "NotNormal"]
+    extremityChoices["leftLower"] = ["Normal", "NotNormal"]
+    extremityChoices["rightLower"] = ["Normal", "NotNormal"]
+    
+    pelvicChoices = ["Stable", "Unstable"]
+    
+    neuroChoices = ["Intact", "Seizures"]
   }
 }
