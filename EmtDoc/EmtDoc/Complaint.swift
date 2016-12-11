a036b9997b8fa635ece57d80606738ab8ace515e
czross <czr@uw.edu>
2016-12-11 11:04:39 -0800

    working on complaint

--- EmtDoc/EmtDoc/Complaint.swift
+++ EmtDoc/EmtDoc/Complaint.swift
@@ -7,3 +7,29 @@
 //
 
 import Foundation
+
+public class {
+    let areaChoice: [String] = [
+        "Head",
+        "Neck",
+        "Upper Back",
+        "Upper Chest",
+        "Lower Back",
+        "Lower Chest",
+        "Right Leg",
+        "Left Leg",
+        "Right Arm",
+        "Left Arm"
+    ]
+    let intensityChoice: [Int] = [1...10]
+    var descritption: String
+    var signsSymptoms: String
+    var bodyArea: String
+    var duration: String
+    var intensity: Int
+    
+    init() {
+        self.description = ""
+    }
+    
+}
