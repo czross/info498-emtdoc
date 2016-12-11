8c33fe494f5e787f55a055a7e418f7ed3bc544c9
czross <czr@uw.edu>
2016-12-11 13:07:31 -0800

    work on ProcedureVC

--- EmtDoc/EmtDoc/ProcedureViewController.swift
+++ EmtDoc/EmtDoc/ProcedureViewController.swift
@@ -8,10 +8,24 @@
 
 import UIKit
 
-class ProcedureViewController: UIViewController {
+class ProcedureViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
+    var EmtDocModel = EmtDoc()
+    var procedures: [String] = []
+    var location: [String] = []
+    
+    @IBOutlet weak var picker: UIPickerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        
+        self.EmtDocModel = appDelegate.EmtDocModel
+        self.procedures = self.EmtDocModel.procedures.getProcedures()
+        self.location = self.EmtDocModel.procedures.getBandageLocation()
+        
+        self.picker.delegate = self
+        self.picker.dataSource = self
 
         // Do any additional setup after loading the view.
     }
@@ -22,6 +36,18 @@ class ProcedureViewController: UIViewController {
     }
     
 
+    public func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 2
+    }
+    
+    public func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return self.procedures.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return self.procedures[row]
+    }
+    
     /*
     // MARK: - Navigation
 
